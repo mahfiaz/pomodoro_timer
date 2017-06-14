0834b7cdbebf8eb008fc63410d9ced2e50b7dc24
Mattias Põldaru <mahfiaz@gmail.com>
2017-06-14 09:53:07 +0300

    Javascript modifications

--- script.js
+++ script.js
@@ -1,79 +1,128 @@
 var cycleCount = 0;
 var timer;
-var toWork = new Audio("back_to_work_Barbossa.mp3");
-var takeBreak = new Audio("break_time.mp3");
+
+var audioWork = new Audio("back_to_work_Barbossa.mp3");
+var audioBreak = new Audio("break_time.mp3");
+
+var isBreak = false;
+var isWork = false;
+
+var start; // milliseconds
+var end; // milliseconds
+var length; // seconds
+var remaining; // seconds
+
+// Default lengths in minutes
+var defaultPomodoro = 25;
+var defaultLongBreak = 15;
+var defaultShortBreak = 5;
+
+var precision = true;
+var pretickLength = 900;
+var tickLength = 10;
+var unpreciseTickLength = 5000;
 
 window.onload = function() {
-	$("start").click();
+    document.addEventListener('visibilitychange', handleVisibilityChange, false);
+
+    $("#start").click();
 };
 
-function getEndTime(minutes) {
-	var starts = new Date().getTime();
-	var time = starts + (1000 * 60 * minutes);
-	var ends = new Date(time);
-	return ends;
+function starting() {
+    var time = Date.now();
+    start = time;
+    end = start + length * 1000;
+    remaining = length;
+    display();
+    timer = window.setInterval(pretick, pretickLength);
 }
 
-function getTimeRemaining(endTime) {
-	var timeArray = [];
-	var now = new Date();
-	var timeLeft = new Date(endTime - now);
-	var minutes = addZeroBefore(timeLeft.getMinutes(), 2);
-	var seconds = addZeroBefore(timeLeft.getSeconds(), 2);
+function pretick() {
+    clearInterval(timer);
+    timer = window.setInterval(tick, tickLength);
+}
 
-	if (minutes == "00" && seconds == "00") {
-		return null;
-	}
+function tick() {
+    var time = Date.now();
+    var diff = Math.floor((end - time) / 1000);
 
-	timeArray.push(minutes);
-	timeArray.push(seconds);
-	
-	return timeArray;
+    if (diff != remaining) {
+        remaining = diff;
+        display();
+
+        // Handle reaching zero
+        if (diff <= 0) {
+            ending();
+            return;
+        }
+
+        clearInterval(timer);
+        if (precision) {
+            timer = window.setInterval(pretick, pretickLength);
+        } else {
+            // Calculate almost exact length for next tick
+            var currentLen = (end - time - 100) % unpreciseTickLength;
+            timer = window.setInterval(tick, currentLen);
+        }
+    }
+}
+
+function ending() {
+    clearInterval(timer);
+
+    if (isWork) {
+        cycleCount += 1;
+        pomodoro_break();
+    }
+    else if (isBreak) {
+        pomodoro();
+    }
+}
+
+function display() {
+    var minutes = Math.floor(remaining / 60);
+    var seconds = remaining % 60;
+    if (minutes < 10) minutes = '0' + minutes;
+    if (seconds < 10) seconds = '0' + seconds;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
 }
 
 function pomodoro() {
-	var pomodoroEnd = getEndTime(25);
-	$("#start").css("visibility", "hidden")
-	$("#task").css("visibility", "visible")
-	$("#timeLeft").css("visibility", "visible")
-	$("#task").html("Get to work!");
-	timer = window.setInterval(function() {
-		var time = getTimeRemaining(pomodoroEnd);
-		if () {
-			$("#minutes").html(time[0]);	
-			$("#seconds").html(time[1]);
-		} else {
-			clearInterval(timer);
-			takeBreak.play();
-			pomodoro_break();
-		}
-	}, 1);
-	cycleCount += 1;
+    isWork = true;
+    isBreak = false;
+    length = 60 * defaultPomodoro;
+    $("#start").css("visibility", "hidden")
+    $("#task").css("visibility", "visible")
+    $("#timeLeft").css("visibility", "visible")
+    $("#task").html("Get to work!");
+    audioWork.play();
+    starting();
+    cycleCount += 1;
 }
 
 function pomodoro_break() {
-	if (cycleCount % 4 != 0) {
-		var breakEnd = getEndTime(5);
-	} else {
-		var breakEnd = getEndTime(15);
-	}
-	$("#task").html("Take a break!");
-	timer = window.setInterval(function() {
-		var time = getTimeRemaining(breakEnd);
-		if (time != null) {
-			$("#minutes").html(time[0]);	
-			$("#seconds").html(time[1]);
-		} else {
-			clearInterval(timer);
-			toWork.play();
-			pomodoro();
-		}
-	}, 1);
+    isBreak = true;
+    isWork = false;
+
+    if (cycleCount % 4 != 0) {
+        // Short break
+        length = 60 * defaultShortBreak;
+    } else {
+        // Long break
+        length = 60 * defaultLongBreak;
+    }
+    $("#task").html("Take a break!");
+    audioBreak.play();
+    starting();
 }
 
-function addZeroBefore(number) {
-	if(number < 10) {
-		number = "0" + number;
-	}
-	return number;
+function handleVisibilityChange() {
+    precision = !document.hidden;
+    if (precision) {
+        tick();
+    } else {
+        clearInterval(timer);
+        timer = window.setInterval(tick, unpreciseTickLength);
+    }
 }
